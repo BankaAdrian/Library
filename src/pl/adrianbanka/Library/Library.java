b231530d4cdc1ddbf351861818c0c258ad960e82
Adrian <banka.adrian@gmail.com>
2022-09-20 16:42:04 +0200

    Dodawanie konstruktorów.

--- src/pl/adrianbanka/Library/Library.java
+++ src/pl/adrianbanka/Library/Library.java
@@ -2,30 +2,10 @@ package pl.adrianbanka.Library;
 
 public class Library {
     public static void main(String[] args) {
-        final String appName = "Biblioteka v.02";
+        final String appName = "Biblioteka v.03";
 
-//        Book book1 = new Book();
-//        book1.title = "W pustyni i w puszczy";
-//        book1.author = "Henryk Sienkiewicz";
-//        book1.releaseDate = 2010;
-//        book1.pages = 296;
-//        book1.publisher = "Greg";
-//        book1.isbn = "9788373271890";
-//
-//
-//        System.out.println(appName);
-//        System.out.println(appName);
-//        System.out.println("Książki dostępne w bibliotece:");
-//        System.out.println(book1.title);
-//        System.out.println(book1.author);
-//        System.out.println(book1.releaseDate);
-//        System.out.println(book1.pages);
-//        System.out.println(book1.publisher);
-//        System.out.println(book1.isbn);
-
-        CreateBook createBook = new CreateBook();
-        Book book = createBook.createBook("heniek", "w pustyni");
-        book.getInfo();
+        Book book1 = new Book("w pustyni sie dupcza", "heniek", 1992, 450, "nie wiem", "3434");
+        book1.printInfo();
 
 
 
