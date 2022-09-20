b6ef29058eb2fbdde52159dd6a76aa2e5b16e9af
Adrian <banka.adrian@gmail.com>
2022-09-20 18:45:31 +0200

    Przeciążanie konstruktora klasy book.

--- src/pl/adrianbanka/Library/Library.java
+++ src/pl/adrianbanka/Library/Library.java
@@ -2,11 +2,13 @@ package pl.adrianbanka.Library;
 
 public class Library {
     public static void main(String[] args) {
-        final String appName = "Biblioteka v.03";
+        final String appName = "Biblioteka v.04";
 
         Book book1 = new Book("w pustyni sie dupcza", "heniek", 1992, 450, "nie wiem", "3434");
         book1.printInfo();
 
+        Book book = new Book("chujnia", "brak autora", 1993, 32, "major");
+        book.printInfo();
 
 
     }
