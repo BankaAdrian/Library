fdb9957fd5c66a7a3c5606dc13004b8ff152da1a
Adrian <banka.adrian@gmail.com>
2022-09-24 12:20:42 +0200

    Specyfikatory dostępu.

--- src/pl/adrianbanka/Library/App/Library.java
+++ src/pl/adrianbanka/Library/App/Library.java
@@ -1,8 +1,10 @@
-package pl.adrianbanka.Library;
+package pl.adrianbanka.Library.App;
+
+import pl.adrianbanka.Library.Model.Book;
 
 public class Library {
     public static void main(String[] args) {
-        final String appName = "Biblioteka v.04";
+        final String appName = "Biblioteka v.05";
 
         Book book1 = new Book("w pustyni sie dupcza", "heniek", 1992, 450, "nie wiem", "3434");
         book1.printInfo();
