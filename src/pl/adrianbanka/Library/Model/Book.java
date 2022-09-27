9ca2854ea12e01c1a517f899be6a6b494e2df798
Adrian <banka.adrian@gmail.com>
2022-09-27 18:14:36 +0200

    Instrukcje sterujące.

--- src/pl/adrianbanka/Library/Model/Book.java
+++ src/pl/adrianbanka/Library/Model/Book.java
@@ -72,6 +72,9 @@ public class Book {
 
     public void printInfo() {
         String info = title + ";" + author + ";" + releaseDate +";" + pages + ";" + publisher + ";" + isbn;
+        if (isbn != null) {
+            info = info + "; " + isbn;
+        }
         System.out.println(info);
     }
 }
