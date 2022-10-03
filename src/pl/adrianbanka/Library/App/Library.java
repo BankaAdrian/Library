aab835de30d3226d37fd37ae49853a49808d8c72
Adrian <banka.adrian@gmail.com>
2022-10-03 17:21:33 +0200

    Tablice.

--- src/pl/adrianbanka/Library/App/Library.java
+++ src/pl/adrianbanka/Library/App/Library.java
@@ -4,13 +4,15 @@ import pl.adrianbanka.Library.Model.Book;
 
 public class Library {
     public static void main(String[] args) {
-        final String appName = "Biblioteka v.05";
+        final String appName = "Biblioteka v.06";
 
-        Book book1 = new Book("w pustyni sie dupcza", "heniek", 1992, 450, "nie wiem", "3434");
-        book1.printInfo();
+        Book[] books = new Book[1000];
 
-        Book book = new Book("chujnia", "brak autora", 1993, 32, "major");
-        book.printInfo();
+        books[0] = new Book("w pustyni sie dupcza", "heniek", 1992, 450, "nie wiem", "3434");
+        books[0].printInfo();
+
+        books[1] = new Book("chujnia", "brak autora", 1993, 32, "major");
+        books[1].printInfo();
 
 
     }
